11020a7415ab8ed761c44c74f050e04078a228be
flairfox <flairfox@gmail.com>
2022-08-22 17:28:01 +0300

    Added access_denied.mp4 and error handler

--- src/main/java/ru/blodge/bserver/commander/telegram/CommanderBot.java
+++ src/main/java/ru/blodge/bserver/commander/telegram/CommanderBot.java
@@ -4,17 +4,26 @@ import com.github.dockerjava.api.model.Container;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.blodge.bserver.commander.docker.DockerApi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 public class CommanderBot extends TelegramLongPollingBot {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommanderBot.class);
     private static final long ADMIN_USER_ID = Long.parseLong(System.getenv("ADMIN_USER_ID"));
+    private static final String ACCESS_DENIED_FILE = "media/access_denied.mp4";
+
+    private static String ACCESS_DENIED_FILE_ID = null;
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -22,26 +31,47 @@ public class CommanderBot extends TelegramLongPollingBot {
 
         long userId = update.getMessage().getFrom().getId();
         if (userId != ADMIN_USER_ID) {
-            LOGGER.warn("User with ID {} is not an administrator", userId);
-
-            SendMessage accessDeniedMessage = new SendMessage();
-            accessDeniedMessage.setChatId(update.getMessage().getChatId());
-            accessDeniedMessage.setText("Вы кто такие?! Я вас не звал?! Идите нахуй!!!");
-
-            try {
-                execute(accessDeniedMessage);
-            } catch (TelegramApiException e) {
-                LOGGER.error("Error while sending response to {}", update.getMessage());
-            }
-
-            return;
+            handleAccessDeniedError(update);
         }
 
-
         List<Container> containers = DockerApi.instance.getContainers();
-        containers.forEach(container -> {
-            System.out.println(container.getNames()[0]);
-        });
+        containers.forEach(container -> System.out.println(container.getNames()[0]));
+    }
+
+    private void handleAccessDeniedError(Update update) {
+        long chatId = update.getMessage().getChatId();
+        long userId = update.getMessage().getFrom().getId();
+
+        LOGGER.warn("User with ID {} is not an administrator", userId);
+
+        try {
+            SendVideo accessDeniedVideo = new SendVideo();
+            InputFile inputFile = new InputFile();
+            if (ACCESS_DENIED_FILE_ID == null) {
+                inputFile.setMedia(loadAccessDeniedFile());
+            } else {
+                inputFile.setMedia(ACCESS_DENIED_FILE_ID);
+            }
+            accessDeniedVideo.setChatId(chatId);
+            accessDeniedVideo.setVideo(inputFile);
+
+            Message sentMessage = execute(accessDeniedVideo);
+            ACCESS_DENIED_FILE_ID = sentMessage.getVideo().getFileId();
+        } catch (FileNotFoundException | URISyntaxException e) {
+            LOGGER.error("Error while retrieving {} file", ACCESS_DENIED_FILE);
+        } catch (TelegramApiException e) {
+            LOGGER.error("Error while sending response to {}", update.getMessage());
+        }
+    }
+
+    private File loadAccessDeniedFile() throws FileNotFoundException, URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(ACCESS_DENIED_FILE);
+        if (resource == null) {
+            throw new FileNotFoundException();
+        }
+
+        return new File(resource.toURI());
     }
 
     @Override
