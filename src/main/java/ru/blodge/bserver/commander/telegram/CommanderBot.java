b5058d3b63ceedd227ef90cd4b21e32af12d6a64
flairfox <flairfox@gmail.com>
2022-08-23 10:24:24 +0300

    Refactoring. Added TelegramUpdateDispatcher and AccessDeniedErrorHandler

--- src/main/java/ru/blodge/bserver/commander/telegram/CommanderBot.java
+++ src/main/java/ru/blodge/bserver/commander/telegram/CommanderBot.java
@@ -1,22 +1,10 @@
 package ru.blodge.bserver.commander.telegram;
 
-import com.github.dockerjava.api.model.Container;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.blodge.bserver.commander.docker.DockerAgent;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import static ru.blodge.bserver.commander.telegram.TelegramBotConfig.ACCESS_DENIED_FILE;
-import static ru.blodge.bserver.commander.telegram.TelegramBotConfig.ADMIN_USER_ID;
 import static ru.blodge.bserver.commander.telegram.TelegramBotConfig.TELEGRAM_BOT_TOKEN;
 import static ru.blodge.bserver.commander.telegram.TelegramBotConfig.TELEGRAM_BOT_USERNAME;
 
@@ -30,8 +18,7 @@ public class CommanderBot extends TelegramLongPollingBot {
         return instance;
     }
 
-
-    private static String ACCESS_DENIED_FILE_ID = null;
+    private final TelegramUpdateDispatcher dispatcher = new TelegramUpdateDispatcher();
 
     private CommanderBot() {
     }
@@ -39,54 +26,7 @@ public class CommanderBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         LOGGER.info("Received new update {}", update);
-
-        if (update.getMessage().getFrom().getId() != ADMIN_USER_ID) {
-            handleAccessDeniedError(update);
-            return;
-        }
-
-        List<Container> containers = DockerAgent.instance().getContainers();
-        containers.forEach(container -> System.out.println(container.getNames()[0]));
-    }
-
-    private void handleAccessDeniedError(Update update) {
-        LOGGER.warn("User with ID {} is not an administrator", update.getMessage().getFrom().getId());
-
-        // Сообщение на которое нужно отправить ответ
-        Message message = update.getMessage();
-
-        // Файл с видео-ответом
-        InputFile inputFile = new InputFile();
-        if (ACCESS_DENIED_FILE_ID == null) {
-            // todo Кэширование в temp
-            // Загружаем файл с видеоответом, если ID не закэширован
-            ClassLoader classLoader = getClass().getClassLoader();
-            try (InputStream inputStream = classLoader.getResourceAsStream(ACCESS_DENIED_FILE)) {
-                inputFile.setMedia(inputStream, ACCESS_DENIED_FILE);
-                sendAccessDeniedResponse(message, inputFile);
-            } catch (IOException e) {
-                LOGGER.error("Error while loading {}", ACCESS_DENIED_FILE);
-            }
-        } else {
-            // Если ID файла был закэширован, просто отправляем его по ID
-            inputFile.setMedia(ACCESS_DENIED_FILE_ID);
-            sendAccessDeniedResponse(message, inputFile);
-        }
-    }
-
-    private void sendAccessDeniedResponse(Message message, InputFile responseFile) {
-        // Создаем сообщение-видеоответ
-        SendVideo accessDeniedVideo = new SendVideo();
-        accessDeniedVideo.setChatId(message.getChatId());
-        accessDeniedVideo.setVideo(responseFile);
-
-        try {
-            Message sentMessage = execute(accessDeniedVideo);
-            // Обновляем в кэше ID файла видеоответа
-            ACCESS_DENIED_FILE_ID = sentMessage.getVideo().getFileId();
-        } catch (TelegramApiException e) {
-            LOGGER.error("Error while sending response to {}", message);
-        }
+        dispatcher.dispatch(update);
     }
 
     @Override
---
+++ src/main/java/ru/blodge/bserver/commander/telegram/handlers/AccessDeniedErrorHandler.java
@@ -0,0 +1,63 @@
+package ru.blodge.bserver.commander.telegram.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.blodge.bserver.commander.telegram.CommanderBot;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static ru.blodge.bserver.commander.telegram.TelegramBotConfig.ACCESS_DENIED_FILE;
+
+public class AccessDeniedErrorHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommanderBot.class);
+
+    private static String ACCESS_DENIED_FILE_ID = null;
+
+    public void handle(Update update) {
+        LOGGER.warn("User with ID {} is not an administrator", update.getMessage().getFrom().getId());
+
+        // Сообщение на которое нужно отправить ответ
+        Message message = update.getMessage();
+
+        // Файл с видео-ответом
+        InputFile inputFile = new InputFile();
+        if (ACCESS_DENIED_FILE_ID == null) {
+            // todo Кэширование в temp
+            // Загружаем файл с видеоответом, если ID не закэширован
+            ClassLoader classLoader = getClass().getClassLoader();
+            try (InputStream inputStream = classLoader.getResourceAsStream(ACCESS_DENIED_FILE)) {
+                inputFile.setMedia(inputStream, ACCESS_DENIED_FILE);
+                sendAccessDeniedResponse(message, inputFile);
+            } catch (IOException e) {
+                LOGGER.error("Error while loading {}", ACCESS_DENIED_FILE);
+            }
+        } else {
+            // Если ID файла был закэширован, просто отправляем его по ID
+            inputFile.setMedia(ACCESS_DENIED_FILE_ID);
+            sendAccessDeniedResponse(message, inputFile);
+        }
+    }
+
+    private void sendAccessDeniedResponse(Message message, InputFile responseFile) {
+        // Создаем сообщение-видеоответ
+        SendVideo accessDeniedVideo = new SendVideo();
+        accessDeniedVideo.setChatId(message.getChatId());
+        accessDeniedVideo.setVideo(responseFile);
+
+        try {
+            Message sentMessage = CommanderBot.instance().execute(accessDeniedVideo);
+            // Обновляем в кэше ID файла видеоответа
+            ACCESS_DENIED_FILE_ID = sentMessage.getVideo().getFileId();
+        } catch (TelegramApiException e) {
+            LOGGER.error("Error while sending response to {}", message);
+        }
+    }
+
+}
